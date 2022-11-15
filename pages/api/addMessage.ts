81ba4265328f1d44c428e09a35a69b53aa6341ad
10Sadique <jafarsjahan@gmail.com>
2022-11-15 17:17:40 +0600

    addMessage post method api endpoint added

---
+++ pages/api/addMessage.ts
@@ -0,0 +1,33 @@
+import { Message } from './../../typings.d';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import redis from '../../redis';
+
+type Data = {
+    message: Message;
+}
+
+type ErrorData = {
+    body: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorData>
+) {
+    if (req.method !== 'POST') {
+        res.status(405).json({ body: 'Method not allowed' })
+        return;
+    }
+
+    const { message } = req.body;
+    const newMessage = {
+        ...message,
+        created_at: Date.now() // replacing the client timestamp with server timestamp
+    }
+
+    // push to upstash redis
+    await redis.hset('messages', message.id, JSON.stringify(newMessage))
+
+    res.status(200).json({ message: newMessage })
+}
