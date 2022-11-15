fddb516ae08eca13795c6b5cf3b76d904f7c7817
10Sadique <jafarsjahan@gmail.com>
2022-11-15 14:04:08 +0600

    project initialized

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+        <>
+            <title>Messenger v2</title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
